3e66cc65e5250553d7521fb78bdfdf220edc0b2b
Anshuman khandka <anshumankhandka2006@outlook.com>
2025-06-12 10:21:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Change navbar background color on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
 
